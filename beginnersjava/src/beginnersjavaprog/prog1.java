a67ec0d150c735603ec71a1642cce6ecdd3488ce
shobhitgunwant <78945880+shobhitgunwant@users.noreply.github.com>
2021-02-18 22:37:11 +0530

    prog done.

---
+++ beginnersjava/src/beginnersjavaprog/prog1.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package beginnersjavaprog;
+
+
+public class prog1 {
+
+	
+	public static void main(String[] args) {
+		int integerone = 10;
+		int integertwo = 20;
+		System.out.println(integerone + integertwo);
+		
+		
+      }
+
+}
