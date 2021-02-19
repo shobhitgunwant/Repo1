e9c6078505ae165dafacc396cff84dfd51b367e6
shobhitgunwant <78945880+shobhitgunwant@users.noreply.github.com>
2021-02-19 09:20:47 +0530

    done

---
+++ beginnersjava/src/beginnersjavaprog/prog5.java
@@ -0,0 +1,11 @@
+package beginnersjavaprog;
+
+public class prog5 {
+
+	public static void main(String[] args) {
+		 int age = 0; 
+	        age = age + 5; 
+	        System.out.println("Student age is : " + age); 
+	}
+
+}
