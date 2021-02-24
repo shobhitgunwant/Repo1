edb229f3ce27cebb7359120fca90f63caf358529
shobhitgunwant <78945880+shobhitgunwant@users.noreply.github.com>
2021-02-24 09:05:10 +0530

    conditional st. done

---
+++ beginnersjava/src/beginnersjavaprog/prog8.java
@@ -0,0 +1,18 @@
+package beginnersjavaprog;
+
+public class prog8
+{
+
+	public static void main(String[] args)
+	
+		{
+			int num1=1;
+			int num2=2;
+			if(num1<num2)		
+			{
+				System.out.print("num1 is smaller than num2");
+			}
+			
+		}
+
+}
