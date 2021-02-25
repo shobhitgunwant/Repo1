15d777f040cd7283b306e98191533613bddf8113
shobhitgunwant <78945880+shobhitgunwant@users.noreply.github.com>
2021-02-25 09:22:42 +0530

    else if

---
+++ beginnersjava/src/beginnersjavaprog/prog9.java
@@ -0,0 +1,25 @@
+package beginnersjavaprog;
+
+public class prog9 
+{
+
+	public static void main(String[] args)
+	{
+		int a =120;
+		if(a< 100)
+		{
+			System.out.println("Variable is less than 100");
+		}
+		else if(a==100)
+		{
+			System.out.println("Variable is equal to 100");
+		}
+		else if (a>100)
+		{
+			System.out.println("Variable is greater than 100");
+		}
+	}
+}
+	
+
+
