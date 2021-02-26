964cbaf14934a3b61fe4b757ed81d60808cdba99
shobhitgunwant <78945880+shobhitgunwant@users.noreply.github.com>
2021-02-26 09:13:05 +0530

    nested if

---
+++ beginnersjava/src/beginnersjavaprog/Prog11.java
@@ -0,0 +1,21 @@
+package beginnersjavaprog;
+
+public class Prog11 
+{
+
+	public static void main(String[] args)
+	{
+		   int num=99;
+			if( num < 100 )
+			      { 
+		           System.out.println("number is less than 100");
+		           }
+		           
+		           if(num > 50)
+		           {
+			      System.out.println("number is greater than 50");
+			      }
+
+	}
+
+}
