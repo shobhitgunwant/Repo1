feab54cbba9370cd6a6b20f9e8c9d64cbee917b2
shobhitgunwant <78945880+shobhitgunwant@users.noreply.github.com>
2021-02-26 09:06:20 +0530

    if else if

---
+++ beginnersjava/src/beginnersjavaprog/Prog10.java
@@ -0,0 +1,23 @@
+package beginnersjavaprog;
+
+public class Prog10 
+{
+
+	public static void main(String[] args)
+	{
+		int number = -10;
+	    if (number > 0) 
+	    {
+	      System.out.println("The number is positive.");
+	    }
+	    else if (number < 0)
+	    {
+	      System.out.println("The number is negative.");
+	    }
+	    else 
+	    {
+	      System.out.println("The number is 0.");
+	    }
+	  }
+}
+
