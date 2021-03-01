0fbc588150603390c1b45258edf8b9a7298b2208
shobhitgunwant <78945880+shobhitgunwant@users.noreply.github.com>
2021-03-01 19:34:20 +0530

    operetors

---
+++ beginnersjava/src/beginnersjavaprog/Prog13.java
@@ -0,0 +1,16 @@
+package beginnersjavaprog;
+
+public class Prog13 
+{
+
+	public static void main(String[] args) 
+	{
+		int integer = 30;
+		int integertwo= 20;
+		integer *= 999;
+		integertwo /= 5;
+		System.out.println(integer);
+		System.out.println(integertwo);
+	}
+
+}
