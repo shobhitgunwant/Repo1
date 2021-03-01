fe8cf243fc599f438cf5cc4ba434435f86b0eba7
shobhitgunwant <78945880+shobhitgunwant@users.noreply.github.com>
2021-03-01 19:05:36 +0530

    relational op.

---
+++ beginnersjava/src/beginnersjavaprog/Prog12.java
@@ -0,0 +1,15 @@
+package beginnersjavaprog;
+
+public class Prog12
+{
+
+	public static void main(String[] args) 
+	{
+		int integer = 20;
+		System.out.print(++integer);
+		System.out.print(integer++);
+		System.out.print(integer);	
+		
+	}
+}
+
